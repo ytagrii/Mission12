996255d8f8026bb993490e062393de47a0ce5f7b
Emma Gardner <emmajoig@gmail.com>
2022-03-18 16:26:42 -0600

    Adding the stuff for the repository and models, no migrations yet

---
+++ Mission12/Models/Signup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission12.Models
+{
+    public class Signup
+    {
+        [Key]
+        [Required]
+        public int SignupID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int TourSlot { get; set; } // numbered 1-12
+    }
+}
---
+++ Mission12/Models/SignupContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission12.Models
+{
+    public class SignupContext : DbContext
+    {
+        public SignupContext()
+        {
+        }
+
+        public SignupContext(DbContextOptions<SignupContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Signup> Signups { get; set; }
+    }
+}
