22b97293d955edfa7ec1e99157067744e95fa140
levimorse <63205032+levimorse@users.noreply.github.com>
2022-03-18 15:24:59 -0600

    Create SignupModel.cs

---
+++ Mission12/Models/SignupModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission12.Models
+{
+    public class SignupModel
+    {
+        [Key]
+        [Required]
+        public int SignupID { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Date { get; set; }
+        
+    }
+}
