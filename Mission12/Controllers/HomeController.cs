aeb9330cf6ec00004abf260b0044b82f0ab8ea74
levimorse <63205032+levimorse@users.noreply.github.com>
2022-03-21 11:01:34 -0600

    Sum

--- Mission12/Controllers/HomeController.cs
+++ Mission12/Controllers/HomeController.cs
@@ -37,10 +37,6 @@ namespace Mission12.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
